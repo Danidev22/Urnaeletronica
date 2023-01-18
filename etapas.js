feca908518bfb8c874ac18658ca66355aa0ba7d9
unknown <daniele.alves.oliveira.2@gmail.com>
2023-01-18 17:47:01 -0300

    enviando codigo

---
+++ etapas.js
@@ -0,0 +1,55 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Carlos Bolsonaro',
+                partido: 'PL',
+                fotos: [
+                    { url: 'carlosbousonaro.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '71717',
+                nome: 'Veronica Costa',
+                partido: 'PSOL',
+                fotos: [
+                    { url: 'Veronica.jpg', legenda: 'Vereadora' }
+                ]
+            },
+
+        ]
+
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Jair Bolsonaro',
+                partido: 'PL',
+                vice: 'Braga Netto',
+                fotos: [
+                    { url: 'presidente1.jpg', legenda: 'Presidente' },
+                    { url: 'presidente1.2.jpg', legenda: 'Vice presidente', small: true }
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Dilma Rousseff',
+                partido: 'PT',
+                vice: 'Temer',
+                fotos: [
+                    { url: 'presidente2.jpg', legenda: 'Presidente' },
+                    { url: 'presidente2.1.jpg', legenda: 'Vice presidente', small: true }
+                ]
+            },
+        ]
+    }
+]
+
+
+
